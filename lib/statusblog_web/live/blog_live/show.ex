c6b78f9ec2b455c7075d741385986ef03db1a506
Kevin Lang <me@gnalck.com>
2021-06-09 20:15:34 -0400

    init impl of blog details editing

--- lib/statusblog_web/live/blog_live/show.ex
+++ lib/statusblog_web/live/blog_live/show.ex
@@ -10,12 +10,33 @@ defmodule StatusblogWeb.BlogLive.Show do
 
   @impl true
   def handle_params(%{"id" => id}, _, socket) do
+    blog = Blogs.get_blog!(id)
     {:noreply,
      socket
-     |> assign(:page_title, page_title(socket.assigns.live_action))
-     |> assign(:blog, Blogs.get_blog!(id))}
+     |> assign(:page_title, "Edit blog")
+     |> assign(:blog, blog)
+     |> assign(:changeset, Blogs.change_blog(blog))}
   end
 
-  defp page_title(:show), do: "Show Blog"
-  defp page_title(:edit), do: "Edit Blog"
+  @impl true
+  def handle_event("validate", %{"blog" => blog_params}, socket) do
+    changeset =
+      socket.assigns.blog
+      |> Blogs.change_blog(blog_params)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign(socket, :changeset, changeset)}
+  end
+
+  def handle_event("save", %{"blog" => blog_params}, socket) do
+    case Blogs.update_blog(socket.assigns.blog, blog_params) do
+      {:ok, _blog} ->
+        {:noreply,
+         socket
+         |> put_flash(:info, "Blog updated successfully")}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, :changeset, changeset)}
+    end
+  end
 end
