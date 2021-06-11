5883badac1a0a87b66a7ed6e74586eb98b8eec6f
Kevin Lang <me@gnalck.com>
2021-06-11 19:16:36 -0400

    finish styling login page

--- lib/statusblog_web/views/layout_view.ex
+++ lib/statusblog_web/views/layout_view.ex
@@ -6,54 +6,22 @@ defmodule StatusblogWeb.LayoutView do
     |> Enum.map(fn {type, msg} -> flash_alert(type, msg) end)
   end
 
-  def flash_alert("info", msg) do
+  def flash_alert(type, msg) do
     ~E"""
     <div
       x-data="{ show: true }"
       x-show.transition="show"
-      x-init="setTimeout(() => show = false, 5000)"
-      class="rounded-md bg-green-50 border border-green-300 p-4 fixed top-4 left-1/2 transform -translate-x-1/2"
+      x-init="setTimeout(() => show = false, 4000)"
+      class="notification is-light <%= flash_alert_class(type) %>"
+      style="top: 5%;left: 50%;transform: translate(-50%);position: fixed;"
     >
-      <div class="flex">
-        <div class="flex-shrink-0">
-          <!-- Heroicon name: solid/check-circle -->
-          <svg class="h-5 w-5 text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-          </svg>
-        </div>
-        <div class="ml-3">
-          <h3 class="text-sm font-medium text-green-800">
-            <%= msg %>
-          </h3>
-        </div>
-      </div>
-    </div>
-
-    """
-  end
-
-  def flash_alert("error", msg) do
-    ~E"""
-    <div
-      x-data="{ show: true }"
-      x-show.transition="show"
-      x-init="setTimeout(() => show = false, 5000)"
-      class="rounded-md bg-red-50 border border-red-300 p-4 fixed top-4 left-1/2 transform -translate-x-1/2"
-    >
-      <div class="flex">
-        <div class="flex-shrink-0">
-          <!-- Heroicon name: solid/x-circle -->
-          <svg class="h-5 w-5 text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
-          </svg>
-        </div>
-        <div class="ml-3">
-          <h3 class="text-sm font-medium text-red-800">
-            <%= msg %>
-          </h3>
-        </div>
-      </div>
+      <button x-on:click="show = false" class="delete"></button>
+      <%= msg %>
     </div>
     """
   end
+
+  defp flash_alert_class("info"), do: "is-info"
+  defp flash_alert_class("error"), do: "is-danger"
+
 end
