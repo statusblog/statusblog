45364d2a2ec00f53369d4de62603bd2a54f51d94
Kevin Lang <me@gnalck.com>
2021-06-11 20:36:22 -0400

    redesign register screen

--- lib/statusblog_web/views/error_helpers.ex
+++ lib/statusblog_web/views/error_helpers.ex
@@ -11,7 +11,7 @@ defmodule StatusblogWeb.ErrorHelpers do
   def error_tag(form, field) do
     Enum.map(Keyword.get_values(form.errors, field), fn error ->
       content_tag(:span, translate_error(error),
-        class: "mt-2 text-sm text-red-600",
+        class: "is-danger help",
         phx_feedback_for: input_name(form, field)
       )
     end)
