37a90e292a3f91a01033c0f713b305d51f02ec5a
Kevin Lang <me@gnalck.com>
2021-06-11 21:08:54 -0400

    add confirmation styling

--- lib/statusblog_web/views/layout_view.ex
+++ lib/statusblog_web/views/layout_view.ex
@@ -12,8 +12,8 @@ defmodule StatusblogWeb.LayoutView do
       x-data="{ show: true }"
       x-show.transition="show"
       x-init="setTimeout(() => show = false, 4000)"
-      class="notification is-light <%= flash_alert_class(type) %>"
-      style="top: 5%;left: 50%;transform: translate(-50%);position: fixed;"
+      class="box notification is-light <%= flash_alert_class(type) %>"
+      style="top: 5%;left: 50%;transform: translate(-50%);position: fixed;z-index: 1;"
     >
       <button x-on:click="show = false" class="delete"></button>
       <%= msg %>
