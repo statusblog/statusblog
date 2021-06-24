d55a16348d1170212e08bedf0850c1d1c6c71cbc
Kevin Lang <me@gnalck.com>
2021-06-24 14:08:19 -0400

    add incident status icons

--- lib/statusblog_web/enum_helpers.ex
+++ lib/statusblog_web/enum_helpers.ex
@@ -40,4 +40,29 @@ defmodule StatusblogWeb.EnumHelpers do
     </svg>
     """
   end
+
+
+  def incident_status_svg(:investigating) do
+    ~E"""
+    <svg class="mr-1.5 w-6 h-6 my-auto text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 16l2.879-2.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242zM21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+    """
+  end
+
+  def incident_status_svg(:identified) do
+    ~E"""
+    <svg class="w-6 h-6 mr-1.5 my-auto text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
+    """
+  end
+
+  def incident_status_svg(:monitoring) do
+    ~E"""
+    <svg class="w-6 h-6 mr-1.5 my-auto text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
+    """
+  end
+
+  def incident_status_svg(:resolved) do
+    ~E"""
+    <svg class="w-6 h-6 mr-1.5 my-auto text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"></path></svg>
+    """
+  end
 end
