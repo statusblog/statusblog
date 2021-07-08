b74b610650fe944b44b70a84d356007090b11e5a
Kevin Lang <me@gnalck.com>
2021-07-08 15:11:22 -0400

    add removal functionality

--- apps/statusblog_web/assets/js/app.js
+++ apps/statusblog_web/assets/js/app.js
@@ -18,7 +18,19 @@ import topbar from "topbar"
 import {LiveSocket} from "phoenix_live_view"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+//let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let liveSocket = new LiveSocket('/live', Socket, {
+  dom: {
+    onBeforeElUpdated(from, to) {
+      if (from.__x) {
+        window.Alpine.clone(from.__x, to)
+      }
+    }
+  },
+  params: {
+    _csrf_token: csrfToken
+  },
+})
 
 // Show progress bar on live navigation and form submits
 topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
--- apps/statusblog_web/lib/statusblog_web/live/subscription_live/index.ex
+++ apps/statusblog_web/lib/statusblog_web/live/subscription_live/index.ex
@@ -11,12 +11,23 @@ defmodule StatusblogWeb.SubscriptionLive.Index do
       |> MountHelpers.assign_defaults(params, session)
       |> assign_subscriptions()
       |> assign(:page_title, "Subscribers")
-      |> assign(:menu, :subscriptions)}
+      |> assign(:menu, :subscriptions)
+      |> assign(:confirm_delete_subscription_id, nil)}
   end
 
   defp assign_subscriptions(socket) do
     socket
     |> assign(:subscriptions, Subscriptions.list_subscriptions(socket.assigns.blog.id))
+  end
+
+  @impl true
+  def handle_event("remove", %{"id" => subscription_id}, socket) do
+    id = String.to_integer(subscription_id)
+    {[subscription_to_delete], subscriptions} = Enum.split_with(socket.assigns.subscriptions, fn x -> x.id == id end)
+    {:ok, _} = Subscriptions.delete_subscription(subscription_to_delete)
+
+    {:noreply,
+      assign(socket, :subscriptions, subscriptions)}
   end
 
 end
