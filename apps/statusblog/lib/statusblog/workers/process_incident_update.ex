72b9aa18756f3f1b417708843a083d9615a58120
Kevin Lang <me@gnalck.com>
2021-07-10 19:24:15 -0400

    pass along the objects verbatim instead of splitting out props

--- apps/statusblog/lib/statusblog/workers/process_incident_update.ex
+++ apps/statusblog/lib/statusblog/workers/process_incident_update.ex
@@ -1,33 +1,28 @@
 defmodule Statusblog.Workers.ProcessIncidentUpdate do
   use Oban.Worker, queue: :default
 
+  alias Statusblog.Blogs
   alias Statusblog.Incidents
   alias Statusblog.Subscriptions
   alias Statusblog.Workers.SendIncidentUpdateEmail
 
   @impl Oban.Worker
-  # def perform(_job) do
-  #   IO.puts "HELLO"
-  #   :ok
-  # end
   def perform(%Oban.Job{args: %{"incident_update_id" => incident_update_id, "is_new?" => is_new?}}) do
     incident_update = Incidents.get_incident_update!(incident_update_id)
     incident = Incidents.get_incident!(incident_update.incident_id)
-
+    blog = Blogs.get_blog!(incident.blog_id)
     subscriptions = Subscriptions.list_subscriptions(incident.blog_id)
+
     for subscription <- subscriptions do
       # right now, we only have email type - when adding more, conver to case statement
       %{
-        update_id: incident_update.id,
-        update_status: incident_update.status,
-        update_components: incident_update.components,
+        blog: blog,
+        incident: incident,
+        incident_update: incident_update,
         is_new?: is_new?,
-        incident_name: incident.name,
-        incident_id: incident.id,
-        subscription_id: subscription.id,
-        subscription_email: subscription.email
+        subscription: subscription
       }
-      |> IO.inspect()
+      IO.inspect()
       # |> SendIncidentUpdateEmail.new()
       # |> Oban.insert!()
     end
